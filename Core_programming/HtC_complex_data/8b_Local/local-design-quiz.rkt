33e5eb049d4469f9c3978f31f45ec8fa5565ddad
Rodrigo Weller Zarrabal <cs.rgzarrabal@gmail.com>
2022-10-11 19:13:06 -0500

    Added - Module 8b design quiz

---
+++ Core_programming/HtC_complex_data/8b_Local/local-design-quiz.rkt
@@ -0,0 +1,147 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname local-design-quiz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; local-design-quiz.rkt
+
+;
+; Problem 1:
+;
+; Suppose you have rosters for players on two opposing tennis team, and each
+; roster is ordered by team rank, with the best player listed first. When both 
+; teams play, the best players of each team play one another,
+; and the second-best players play one another, and so on down the line. When
+; one team has more players than the other, the lowest ranking players on
+; the larger team do not play.
+;
+; Design a function that consumes two rosters, and produces true if all players 
+; on both teams will play if the teams play each other. 
+; No marks will be given to solution that do not use a cross product table. 
+;
+
+;; Player is String
+;; interp.  the name of a tennis player.
+(define P0 "Maria")
+(define P2 "Serena")
+
+#;
+(define (fn-for-player p)
+  (... p))
+
+
+
+;; Roster is one of:
+;; - empty
+;; - (cons Player Roster)
+;; interp.  a team roster, ordered from best player to worst.
+(define R0 empty)
+(define R1 (list "Eugenie" "Gabriela" "Sharon" "Aleksandra"))
+(define R2 (list "Maria" "Nadia" "Elena" "Anastasia" "Svetlana"))
+
+#;
+(define (fn-for-roster r)
+  (cond [(empty? r) (...)]
+        [else 
+         (... (fn-for-player (first r))
+              (fn-for-roster (rest r)))]))
+
+
+
+(define-struct match (p1 p2))
+;; Match is (make-match Player Player)
+;; interp.  a match between player p1 and player p2, with same team rank
+(define M0 (make-match "Eugenie" "Maria"))
+(define M1 (make-match "Gabriela" "Nadia"))
+
+#;
+(define (fn-for-match m)
+  (... (match-p1 m) (match-p2 m)))
+
+
+
+;; ListOfMatch is one of:
+;; - empty
+;; - (cons Match ListOfMatch)
+;; interp. a list of matches between one team and another.
+(define LOM0 empty)
+(define LOM1 (list (make-match "Eugenie" "Maria")
+                   (make-match "Gabriela" "Nadia")))
+
+#;
+(define (fn-for-lom lom)
+  (cond [(empty? lom) (...)]
+        [else
+         (... (fn-for-match (first lom))
+              (fn-for-lom (rest lom)))]))
+
+
+;; ==============
+;; Functions
+
+;; Roster Roster -> Boolean
+;; produces true if all players on both teams will play if the teams play each other
+
+;; Cross product
+
+;;                                        r2
+;;                          ┌─────────┬───────────────────────────┐
+;;                          │  empty  │   (cons Player Roster)    │
+;;     ┌────────────────────┼─────────┼───────────────────────────┤
+;;     │       empty        │  true(1)│           false (2)       │
+;;   r ├────────────────────┼─────────┼───────────────────────────┤
+;;   1 │(cons Player Roster)│ false(2)│    (check the <rests>) (4)│
+;;     └────────────────────┴─────────┴───────────────────────────┘
+
+(check-expect (all-roster-play? empty empty) true)
+(check-expect (all-roster-play? empty R1) false)
+(check-expect (all-roster-play? R1 empty) false)
+(check-expect (all-roster-play? R1 R1) true)
+(check-expect (all-roster-play? R1 R2) false)
+
+;(define (all-roster-play? r1 r2) false)  ;stub
+
+(define (all-roster-play? r1 r2)
+  (cond [(and (empty? r1) (empty? r2)) true]    ; 1
+        [(or  (empty? r1) (empty? r1)) false]   ; 2
+        [else                                   ; 4
+         (all-roster-play? (rest r1) (rest r2))]))
+
+
+;
+; Problem 2:
+;
+; Now write a function that, given two teams, produces the list of tennis matches
+; that will be played. 
+;
+; Assume that this function will only be called if the function you designed above
+; produces true. In other words, you can assume the two teams have the same number
+; of players. 
+;
+
+;; Roster Roster -> ListOfMatch
+;; produces the list of tennis matches that will be played
+;; ASSUME: both rosters will have the same length
+
+;; Cross product
+
+;;                                        r2
+;;                          ┌─────────┬───────────────────────────┐
+;;                          │  empty  │   (cons Player Roster)    │
+;;     ┌────────────────────┼─────────┼───────────────────────────┤
+;;     │       empty        │ empty(1)│             x             │
+;;   r ├────────────────────┼─────────┼───────────────────────────┤
+;;   1 │(cons Player Roster)│    x    │(cons (make-match <firsts>)│
+;;     │                    │         │(2)   (list-matches <rest>)│
+;;     └────────────────────┴─────────┴───────────────────────────┘
+
+(check-expect (list-matches empty empty) empty)
+(check-expect (list-matches (list "Eugenie" "Gabriela")
+                            (list "Maria" "Nadia")) (list (make-match "Eugenie" "Maria")
+                                                          (make-match "Gabriela" "Nadia")))
+
+;(define (list-matches r1 r2) empty)  ;stub
+
+(define (list-matches r1 r2)
+  (cond [(empty? r1) empty]              ; 1
+        [else                            ; 2
+         (cons (make-match (first r1) (first r2))
+               (list-matches (rest r1) (rest r2)))]))
